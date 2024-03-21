d7e895cd302b83d26deb4e05fdca92890083b713
shifang0511 <lizhongyu2005@gmail.com>
2024-03-21 17:02:42 +0800

    Add function to calculate difference between elements in array

--- difference.cpp
+++ difference.cpp
@@ -2,11 +2,32 @@
 using namespace std;
 
 const int N=1e6+10;
+int a[N],d[N],n,m;
 
+void cal(int a[],int l,int r,int c)
+{
+    a[l]+=c;
+    a[r+1]-=c;
+}
 
 int main()
 {
-
-
-
+    cin>>n>>m;a[0]=0;
+    for(int i=1;i<=n;i++)
+    {
+        scanf("%d",&a[i]);
+        d[i]=a[i]-a[i-1];
+    }
+    for(int i=1;i<=m;i++)
+    {
+        int l,r,c;
+        scanf("%d%d%d",&l,&r,&c);
+        cal(d,l,r,c);
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        a[i] = d[i] + a[i - 1];    //前缀和运算
+        printf("%d ", a[i]);
+    }
+    return 0;
 }
