f5decb79bbb59c98f943a2b10aba5fd0ee09eb93
shifang0511 <lizhongyu2005@gmail.com>
2024-03-28 12:07:37 +0800

    Refactor KMP algorithm implementation

--- 2.data_structure/__KMP__.cpp
+++ 2.data_structure/__KMP__.cpp
@@ -1,10 +1,32 @@
-#include<iostream>
-#include<string>
-using namespace std;
-const int N=1e5+10;
-string s,p;int ne[N];
+#include <string>
+#include <iostream>
 
-void init()
+using namespace std;
+const int N = 1000010;
+
+string p, s;int a,b;
+int ne[N];
+
+int main()
 {
-    
+    cin >>a>> p>>b >>  s;
+    ne[0] = 0;
+    ne[1] = 0;
+    for (int i = 2,j=0; i < p.size() + 1; i++)
+    {
+        while (j && p[i - 1] != p[j]) j = ne[j];
+        if (p[i - 1] == p[j]) j++;
+        ne[i] = j;
+    }
+    for (int i = 0, j = 0; i < s.size(); i++)
+    {
+        while (j && s[i] != p[j]) j = ne[j];
+        if (s[i] == p[j]) j++;
+        if (j == p.size())
+        {
+            cout << i - j + 1 << " ";
+            
+        }
+    }
+    return 0;
 }
