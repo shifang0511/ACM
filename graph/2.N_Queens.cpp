69aa74afe9646159f68c7e441f729cb2b376d7d1
shifang0511 <lizhongyu2005@gmail.com>
2024-04-01 23:27:47 +0800

    Fix N value and correct loop index in N Queens algorithm

--- graph/2.N_Queens.cpp
+++ graph/2.N_Queens.cpp
@@ -1,21 +1,24 @@
 #include<iostream>
 using namespace std;
-const int N=11;
-char path[N][N],n;
+const int N=10;
+char path[N][N];int n;
 bool row[N],dg[2*N],udg[2*N];
 void dfs(int x){
     if(x==n){
-        for(int i=0;i<n;i++)
-            for(int j=0;i<n;i++)cout<<path[i][j];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){cout<<path[i][j];}
+        cout<<endl;
+        }
         cout<<endl;
         return ;
     }
-    for(int i=1;i<=n;i++){
-        if(!row[i]&&!dg[i]&&!udg[i]){
+    for(int i=0;i<n;i++){
+        if(!row[i]&&!dg[i+x]&&!udg[n-i+x]){
             path[x][i]='Q';
-            row[i]=dg[i+x]=udg[n-i-x]=true;
+            row[i]=dg[i+x]=udg[n-i+x]=true;
             dfs(x+1);
-            row[i]=dg[i+x]=udg[n-i-x]=false;
+            row[i]=dg[i+x]=udg[n-i+x]=false;
+            path[x][i]='.';
         }
     }
 }
