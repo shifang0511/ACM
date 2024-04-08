8d1e3ae5f01e1787a8cf9705b54ed2bb6e7b49f9
shifang0511 <lizhongyu2005@gmail.com>
2024-04-08 19:17:35 +0800

    dfs

---
+++ 3.graph/the _heart_of_tree.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+const int N=1e5+10;int ans=N;
+int n,m;
+int h[N],e[N],ne[N],idx;bool st[N];
+void add(int a,int b){
+    e[idx]=b;ne[idx]=h[a];h[a]=idx++;
+}
+int dfs(int u)
+{
+    int sum=1,res=0;
+    st[u]=true;
+    for(int i=h[u];i!=-1;i=ne[i]){
+        int j=e[i];
+        if(!st[j]){
+            int s=dfs(j);
+            res=max(res,s);
+            sum+=s;
+        }
+    }
+    res=max(res,ans);
+    ans=max(res,n-sum);
+    return sum;
+}
+int main()
+{
+    cin>>n;
+    memset(h,-1,sizeof h);
+    for(int i=0;i<-1;i++){
+        int a,b;cin>>a>>b;
+        add(a,b);add(b,a);
+    }
+    dfs(1);
+    cout<<ans;
+}
