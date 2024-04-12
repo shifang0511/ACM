5c51fa2edfa43a89a79381b8ccf314d9a64f026d
shifang0511 <lizhongyu2005@gmail.com>
2024-04-12 20:38:38 +0800

    1

---
+++ 4.math/13.Linear_congruence_equation.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int exgcd(int a,int b,int& x,int& y)
+{
+    if(b==0){
+        x=1;y=0;
+        return a;
+    }
+    int x1,y1;
+    int d=exgcd(b,a%b,x1,y1);
+    x = y1, y = x1 - a/b*y1;
+    return d;
+}
+
+int main()
+{
+    int m;cin>>m;
+    while(m--)
+    {
+        int a,b,m;
+        cin>>a>>b>>m;
+        if(a%m==0)cout<<"impossible"<<endl;
+        else exgcd(a,b,x,y);
+        cout<<x<<" "<<y<<endl;
+    }
+
+}   
