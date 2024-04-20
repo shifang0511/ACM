faea8dd54e33d969209d72e673bf01ad93f6f3da
shifang0511 <lizhongyu2005@gmail.com>
2024-04-20 19:41:14 +0800

    1

--- 4.math/24.Steps_NimGame.cpp
+++ 4.math/24.Steps_NimGame.cpp
@@ -1,7 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+const int N=1e5+10;
+int n,res;
+int a[N];
 int main()
 {
-    
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        cin>>a[i];
+        if(i%2==1)res^=a[i];
+    }
+    if(!res)cout<<"No"<<endl;
+    else cout<<"Yes"<<endl;
 }
---
+++ 4.math/25.Set_NimGame.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int M=1e4+10,N=110;
+int n,m,res;
+int s[M],sg[N];
+int main()
+{
+    cin>>n;
+    for(int i=0;i<n;i++)cin>>s[i];
+
+}
